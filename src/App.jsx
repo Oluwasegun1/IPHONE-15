778b7bceca23690fe9d16e2fb27e9e59f9dd68dd
Oluwasegun Ogunbanjo <ogunbanjosegun@gmail.com>
2024-11-28 22:58:53 +0100

    npd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
+import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,8 +13,10 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <Footer />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,10 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://bb8a39dd148f848d7c684c06ec6bdfbc@o4508377341493248.ingest.us.sentry.io/4508377347325952",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-
-  replaysSessionSampleRate: 0.1,
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 createRoot(document.getElementById("root")).render(
