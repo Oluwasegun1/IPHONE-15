6c3dc3ed23dc5de17aeff7072124a6bb0be71ec0
Oluwasegun Ogunbanjo <ogunbanjosegun@gmail.com>
2024-11-28 19:44:17 +0100

    bv

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://bb8a39dd148f848d7c684c06ec6bdfbc@o4508377341493248.ingest.us.sentry.io/4508377347325952",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
